df578ec6965b05f13744a786514db1003cf79d27
Davis Allen <davissallen@gmail.com>
2017-09-09 18:42:31 -0700

    adds initial android project

---
+++ app/src/main/java/me/davisallen/oneanddone/MainActivity.java
@@ -0,0 +1,44 @@
+package me.davisallen.oneanddone;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_daily_goal:
+                    mTextMessage.setText(R.string.title_daily_goal);
+                    return true;
+                case R.id.navigation_list:
+                    mTextMessage.setText(R.string.title_list);
+                    return true;
+                case R.id.navigation_calendar:
+                    mTextMessage.setText(R.string.title_calendar);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
