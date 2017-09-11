d84107e4cdef8b474a3d9bf491901dd9c8e827c5
Davis Allen <davissallen@gmail.com>
2017-09-10 19:56:21 -0700

    Add templates for all views

---
+++ app/src/main/java/me/davisallen/oneanddone/ProgressListFragment.java
@@ -0,0 +1,12 @@
+package me.davisallen.oneanddone;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Package Name:   me.davisallen.oneanddone
+ * Project:        one-and-done
+ * Created by davis, on 9/10/17
+ */
+
+public class ProgressListFragment extends Fragment {
+}
