ee91f69bcaaa1d00ac2aa82e25f23ec5389928ee
Davis Allen <davissallen@gmail.com>
2017-09-10 18:19:10 -0700

    Imports Timber library, adds some fragment layouts

--- app/src/main/java/me/davisallen/oneanddone/MainActivity.java
+++ app/src/main/java/me/davisallen/oneanddone/MainActivity.java
@@ -7,38 +7,45 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import timber.log.Timber;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        // Set up Timber DebugTree
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Timber.d("Timberrrrr: you just pressed %d", item.getItemId());
+
             switch (item.getItemId()) {
-                case R.id.navigation_daily_goal:
-                    mTextMessage.setText(R.string.title_daily_goal);
-                    return true;
                 case R.id.navigation_list:
-                    mTextMessage.setText(R.string.title_list);
                     return true;
                 case R.id.navigation_calendar:
-                    mTextMessage.setText(R.string.title_calendar);
                     return true;
             }
+
+
             return false;
         }
     };
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-    }
 
 }
