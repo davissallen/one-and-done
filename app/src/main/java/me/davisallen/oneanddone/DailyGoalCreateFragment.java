623125ad191e2e28c9ce6579c5d7617c8ea1953e
Davis Allen <davissallen@gmail.com>
2017-09-16 15:55:18 -0700

    Updates create goal fragment

--- app/src/main/java/me/davisallen/oneanddone/DailyGoalCreateFragment.java
+++ app/src/main/java/me/davisallen/oneanddone/DailyGoalCreateFragment.java
@@ -30,7 +30,7 @@ public class DailyGoalCreateFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_daily_goal_create, container, false);
+        View view = inflater.inflate(R.layout.fragment_goal_create, container, false);
         ButterKnife.bind(this, view);
         return view;
     }
