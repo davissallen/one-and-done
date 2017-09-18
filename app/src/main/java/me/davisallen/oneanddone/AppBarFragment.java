79eced61c44bd39847d9d7a3bcda3b43ab322edc
Davis Allen <davissallen@gmail.com>
2017-09-17 19:17:39 -0700

    Adds app bar custom fragment

---
+++ app/src/main/java/me/davisallen/oneanddone/AppBarFragment.java
@@ -0,0 +1,12 @@
+package me.davisallen.oneanddone;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Package Name:   me.davisallen.oneanddone
+ * Project:        one-and-done
+ * Created by davis, on 9/17/17
+ */
+
+public class AppBarFragment extends Fragment {
+}
