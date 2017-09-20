5aa4c1468d850987ae5cb56ae8f1df927f6920f1
Davis Allen <davissallen@gmail.com>
2017-09-19 21:46:39 -0700

    Creates skeleton for Firebase user sign in. User can sign in!

--- app/src/main/java/me/davisallen/oneanddone/MainActivity.java
+++ app/src/main/java/me/davisallen/oneanddone/MainActivity.java
@@ -2,10 +2,13 @@ package me.davisallen.oneanddone;
 
 import android.animation.ObjectAnimator;
 import android.animation.StateListAnimator;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
@@ -16,6 +19,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
 import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -45,6 +51,9 @@ public class MainActivity extends AppCompatActivity implements
     @BindView(R.id.app_bar_layout) AppBarLayout mAppBarLayout;
 
     FragmentManager mFragmentManager;
+
+    // Choose an arbitrary request code value
+    private static final int REQUEST_CODE_SIGN_IN = 1738;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -81,7 +90,7 @@ public class MainActivity extends AppCompatActivity implements
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
-        // TODO: Do something with currentUser
+        updateUi(currentUser);
         // See FirebaseAssistant tool on auth
         // See getting started info here: https://firebase.goo gle.com/docs/auth/android/start
     }
@@ -145,6 +154,58 @@ public class MainActivity extends AppCompatActivity implements
         }
         mToolbar.setTitle("");
         mToolbar.setSubtitle("");
+    }
+
+    private void updateUi(FirebaseUser currentUser) {
+        if (currentUser == null) {
+            // TODO: use smart lock for production
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setIsSmartLockEnabled(true)
+                            .build(),
+                    REQUEST_CODE_SIGN_IN);
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            // Successfully signed in
+            if (resultCode == RESULT_OK) {
+//                startActivity(SignedInActivity.createIntent(this, response));
+//                finish();
+//                return;
+            } else {
+                // Sign in failed
+                if (response == null) {
+                    // User pressed back button
+                    showSnackbar(R.string.sign_in_cancelled);
+                    return;
+                }
+
+                if (response.getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    showSnackbar(R.string.no_internet_connection);
+                    return;
+                }
+
+                if (response.getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                    showSnackbar(R.string.unknown_error);
+                    return;
+                }
+            }
+
+            showSnackbar(R.string.unknown_sign_in_response);
+        }
+    }
+
+    private void showSnackbar(int stringId) {
+        String message = getString(stringId);
+        Snackbar snackbar = Snackbar.make(new CoordinatorLayout(this), message, Snackbar.LENGTH_LONG);
+        snackbar.show();
     }
 
     private void initializeNavigationMenu() {
