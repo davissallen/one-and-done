dc700e1839948826d6731aad0afc1188766c6f21
Davis Allen <davissallen@gmail.com>
2017-12-12 01:08:28 -0800

    Updates calendar counts

--- app/src/main/java/me/davisallen/oneanddone/CalendarFragment.java
+++ app/src/main/java/me/davisallen/oneanddone/CalendarFragment.java
@@ -6,9 +6,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
 import android.widget.TextView;
 
-import java.util.ArrayList;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -22,14 +27,16 @@ import me.davisallen.oneanddone.pojo.Goal;
 
 public class CalendarFragment extends Fragment {
 
-    private static final String GOALS_KEY = "goals_key";
+    private static final String CALENDAR_KEY = "calendar_key";
 
+    @BindView(R.id.calendarView) CalendarView mCalendar;
     @BindView(R.id.count_completed) TextView mCountCompleted;
     @BindView(R.id.count_neutral) TextView mCountNeutral;
     @BindView(R.id.count_uncompleted) TextView mCountUncompleted;
 
-    private ArrayList<Goal> mGoals;
     private MainActivity mParentActivity;
+
+    // TODO: Update the view on every calendar swipe (asynchronously).
 
     @Nullable
     @Override
@@ -38,16 +45,98 @@ public class CalendarFragment extends Fragment {
         ButterKnife.bind(this, view);
 
         mParentActivity = (MainActivity) getActivity();
-
         updateCountViews();
 
         return view;
     }
 
     private void updateCountViews() {
-        // TODO: Update calendar stats.
-        mCountCompleted.setText("Do");
-        mCountNeutral.setText("Re");
-        mCountUncompleted.setText("Mi");
+        long dateInMillis = mCalendar.getDate();
+
+        long begOfMonthInMillis = getBeginningOfMonth(dateInMillis);
+        long endOfMonthInMillis = getEndOfMonth(dateInMillis);
+
+        int count_completed = 0;
+        int count_neutral = Math.round((endOfMonthInMillis - begOfMonthInMillis) / 1000f / 60f / 60f / 24f);
+        int count_uncompleted = 0;
+        for (Goal goal : mParentActivity.mGoals) {
+            long goalMillis = goal.getDateInMillis();
+            if (goalMillis >= begOfMonthInMillis && goalMillis <= endOfMonthInMillis) {
+                if (goal.getIsCompleted()) {
+                    count_completed += 1;
+                } else {
+                    count_uncompleted += 1;
+                }
+                count_neutral -= 1;
+            }
+        }
+
+        updateUI(count_completed, count_neutral, count_uncompleted);
+    }
+
+    private void updateUI(int count_completed, int count_neutral, int count_uncompleted) {
+        mCountCompleted.setText(String.valueOf(count_completed));
+        mCountNeutral.setText(String.valueOf(count_neutral));
+        mCountUncompleted.setText(String.valueOf(count_uncompleted));
+    }
+
+    private long getBeginningOfMonth(long dateInMillis) {
+        // Get month from millis
+        Calendar c = Calendar.getInstance();
+        c.setTimeInMillis(dateInMillis);
+        String yyyy = String.valueOf(c.get(Calendar.YEAR));
+        String mm  = String.valueOf(c.get(Calendar.MONTH) + 1);
+
+        // Get beginning of month in millis
+        String myDate = String.format("%s/%s/01 00:00:00", yyyy, mm);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss", Locale.US);
+        Date date = null;
+        try {
+            date = sdf.parse(myDate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        long begOfMonthInMillis = 0;
+        if (date != null) {
+            begOfMonthInMillis = date.getTime();
+        }
+        return begOfMonthInMillis;
+    }
+
+    private long getEndOfMonth(long dateInMillis) {
+        // Get month from millis
+        Calendar c = Calendar.getInstance();
+        c.setTimeInMillis(dateInMillis);
+
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH) + 1;
+
+        if (month == 12) {
+            // If it was December, get the Jan of next year.
+            year += 1;
+            month = 1;
+        } else {
+            month += 1;
+        }
+
+        String yyyy = String.valueOf(year);
+        String mm  = String.valueOf(month);
+
+        // Get beginning of month in millis
+        String myDate = String.format("%s/%s/01 00:00:00", yyyy, mm);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss", Locale.US);
+        Date date = null;
+        try {
+            date = sdf.parse(myDate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        long endOfMonthInMillis = 0;
+        if (date != null) {
+            // Get the start of next month - 1 == very end of last month.
+            endOfMonthInMillis = date.getTime() - 1;
+        }
+
+        return endOfMonthInMillis;
     }
 }
