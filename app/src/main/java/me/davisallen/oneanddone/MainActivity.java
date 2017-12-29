2bd4068aee238348e2626724b8bbc3d830a2c6df
Davis Allen <davissallen@gmail.com>
2017-12-28 17:41:49 -0800

    Remove settings icon

--- app/src/main/java/me/davisallen/oneanddone/MainActivity.java
+++ app/src/main/java/me/davisallen/oneanddone/MainActivity.java
@@ -194,21 +194,6 @@ public class MainActivity extends AppCompatActivity implements
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle activity_main_drawer view item clicks here.
         int id = item.getItemId();
