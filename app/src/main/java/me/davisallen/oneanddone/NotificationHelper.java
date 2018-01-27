2b12b5fce24e1f59f149f610495616f145a4ffab
Davis Allen <davissallen@gmail.com>
2018-01-27 15:14:17 -0800

    Notification queries database for most recent goal to display

--- app/src/main/java/me/davisallen/oneanddone/NotificationHelper.java
+++ app/src/main/java/me/davisallen/oneanddone/NotificationHelper.java
@@ -11,9 +11,11 @@ import android.support.v4.app.NotificationCompat;
  * Helper class to manage notification channels, and create notifications.
  */
 class NotificationHelper extends ContextWrapper {
+    private static final String CHANNEL_CREATE_GOAL = "channel_create_goal";
+    private static final String CHANNEL_COMPLETE_GOAL = "channel_complete_goal";
+
     private NotificationManager manager;
 
-    public static final String CHANNEL_REMINDER_NAME = "reminder";
     public static final int NOTIFICATION_CREATE_GOAL = 100;
     public static final int NOTIFICATION_COMPLETE_GOAL = 101;
 
@@ -29,30 +31,37 @@ class NotificationHelper extends ContextWrapper {
             return;
         }
 
-        // Create reminder notification channel
-        NotificationChannel reminder_channel = new NotificationChannel(
-                CHANNEL_REMINDER_NAME,
-                getString(R.string.notification_channel1),
+        NotificationChannel createGoalChannel = new NotificationChannel(
+                CHANNEL_CREATE_GOAL,
+                getString(R.string.channel_create_goal),
                 NotificationManager.IMPORTANCE_DEFAULT);
-        reminder_channel.setLightColor(getColor(R.color.colorPrimary));
-        reminder_channel.setDescription(getString(R.string.notif_channel_reminder_description));
-        reminder_channel.setName(CHANNEL_REMINDER_NAME);
-        reminder_channel.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
-        getManager().createNotificationChannel(reminder_channel);
+        createGoalChannel.setLightColor(getColor(R.color.colorPrimary));
+        createGoalChannel.setDescription(getString(R.string.channel_create_goal_desc));
+        createGoalChannel.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
+        getManager().createNotificationChannel(createGoalChannel);
+
+        NotificationChannel completeGoalChannel = new NotificationChannel(
+                CHANNEL_COMPLETE_GOAL,
+                getString(R.string.channel_complete_goal),
+                NotificationManager.IMPORTANCE_DEFAULT);
+        completeGoalChannel.setLightColor(getColor(R.color.colorAccent));
+        completeGoalChannel.setDescription(getString(R.string.channel_complete_goal_desc));
+        completeGoalChannel.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
+        getManager().createNotificationChannel(completeGoalChannel);
     }
 
     public NotificationCompat.Builder getNotificationCreateGoal() {
-        return new NotificationCompat.Builder(getApplicationContext(), CHANNEL_REMINDER_NAME)
-                .setContentTitle("Placeholder to create goal")
-                .setContentText("Placeholder to create goal")
+        return new NotificationCompat.Builder(getApplicationContext(), CHANNEL_CREATE_GOAL)
+                .setContentTitle("COME HERE YA BOY MAKE A GOAL ALREDY K")
+                .setContentText("COME HERE YA BOY MAKE A GOAL ALREDY K")
                 .setSmallIcon(getSmallIcon())
                 .setAutoCancel(true);
     }
 
-    public NotificationCompat.Builder getNotificationCompleteGoal() {
-        return new NotificationCompat.Builder(getApplicationContext(), CHANNEL_REMINDER_NAME)
-                .setContentTitle("Placeholder to complete goal")
-                .setContentText("Placeholder to complete goal")
+    public NotificationCompat.Builder getNotificationCompleteGoal(String goal) {
+        return new NotificationCompat.Builder(getApplicationContext(), CHANNEL_COMPLETE_GOAL)
+                .setContentTitle(goal)
+                .setContentText("DID YOU DO IT YET HUH?")
                 .setSmallIcon(getSmallIcon())
                 .setAutoCancel(true);
     }
--- app/src/main/java/me/davisallen/oneanddone/NotificationJobService.java
+++ app/src/main/java/me/davisallen/oneanddone/NotificationJobService.java
@@ -1,13 +1,25 @@
 package me.davisallen.oneanddone;
 
+import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationCompat;
+import android.text.format.DateUtils;
 
 import com.firebase.jobdispatcher.JobParameters;
 import com.firebase.jobdispatcher.JobService;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.NoSuchElementException;
 
+import me.davisallen.oneanddone.pojo.Goal;
+import me.davisallen.oneanddone.utils.FirebaseUtils;
 import timber.log.Timber;
 
 /**
@@ -19,6 +31,8 @@ import timber.log.Timber;
 public class NotificationJobService extends JobService {
 
     NotificationHelper mNotificationHelper;
+    private DatabaseReference mGoalsByUserDbReference;
+    private FirebaseUser mUser;
 
     @Override
     public boolean onStartJob(JobParameters job) {
@@ -35,9 +49,21 @@ public class NotificationJobService extends JobService {
             }
         }).start();
 
-        mNotificationHelper = new NotificationHelper(getApplicationContext());
+        // Get the user info from FirebaseAuth.
+        mUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (mUser == null) {
+            Timber.e(getResources().getString(R.string.firebase_auth_error));
+        } else {
+            Timber.d(String.format("Actually got a user wow: %s.", mUser.getUid()));
 
-        postNotification(NotificationHelper.NOTIFICATION_CREATE_GOAL);
+        }
+
+        // Obtain the FirebaseStorage instance.
+        FirebaseDatabase firebaseDatabase = FirebaseUtils.getDatabase();
+        mGoalsByUserDbReference = firebaseDatabase.getReference(getString(R.string.goals_db_name)).child(mUser.getUid());
+        mGoalsByUserDbReference.orderByChild("dateInMillis").limitToLast(1).addListenerForSingleValueEvent(getMostRecentGoalByUser);
+
+        mNotificationHelper = new NotificationHelper(getApplicationContext());
 
         return false; // Answers the question: "Is there still work going on?"
     }
@@ -48,7 +74,7 @@ public class NotificationJobService extends JobService {
     }
 
     /** Post the notifications */
-    public void postNotification(int id) {
+    public void postNotification(int id, @Nullable String goal) {
         NotificationCompat.Builder notificationBuilder = null;
         switch (id) {
             case NotificationHelper.NOTIFICATION_CREATE_GOAL:
@@ -56,7 +82,7 @@ public class NotificationJobService extends JobService {
                 break;
 
             case NotificationHelper.NOTIFICATION_COMPLETE_GOAL:
-                notificationBuilder = mNotificationHelper.getNotificationCompleteGoal();
+                notificationBuilder = mNotificationHelper.getNotificationCompleteGoal(goal);
                 break;
         }
 
@@ -64,4 +90,27 @@ public class NotificationJobService extends JobService {
             mNotificationHelper.notify(id, notificationBuilder);
         }
     }
+
+    final ValueEventListener getMostRecentGoalByUser = new ValueEventListener() {
+        @Override
+        public void onDataChange(DataSnapshot dataSnapshot) {
+            Goal goal = null;
+            try {
+                goal = dataSnapshot.getChildren().iterator().next().getValue(Goal.class);
+            } catch (NoSuchElementException e) {
+                Timber.e("No goal was found in the database.");
+                e.printStackTrace();
+                return;
+            }
+            if (goal == null || !DateUtils.isToday(goal.getDateInMillis())) {
+                postNotification(NotificationHelper.NOTIFICATION_CREATE_GOAL, null);
+            } else {
+                postNotification(NotificationHelper.NOTIFICATION_COMPLETE_GOAL, goal.getGoal());
+            }
+        }
+
+        @Override
+        public void onCancelled(DatabaseError databaseError) {
+        }
+    };
 }
